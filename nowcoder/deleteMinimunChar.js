19a0063a7d8d3796b5bceeb8a3aa3fc3d74e5e61
protoy <37529943+protoy@users.noreply.github.com>
2020-07-08 20:16:07 +0800

    delete exercise on nowcoder

--- nowcoder/deleteMinimunChar.js
+++
@@ -1,6 +0,0 @@
-        var line='abcdd';
-        var arr = line.split('');
-        var temp = new Array();
-        for(var i=0; i<arr.length; i++){
-
-        }
--- nowcoder/rabbit.js
+++
@@ -1,9 +0,0 @@
-        month = 9;
-        function count(month){
-            if(month<3){
-                return 1;
-            }else{
-                return count(month-1) + count(month-2);
-            }
-        }
-        console.log(count(month));
